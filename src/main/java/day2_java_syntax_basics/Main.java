ec5d07656ce063acbc9b7f9327fe76330d6b05d5
Godwy <godwynnejoshuadimayuga@gmail.com>
2024-11-16 01:19:08 +0800

    ok

--- src/main/java/day2_java_syntax_basics/Main.java
+++ src/main/java/day2_java_syntax_basics/Main.java
@@ -2,6 +2,9 @@ package day2_java_syntax_basics;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome Joshua to Java!.");
+        System.out.println("hello world");
     }
 }
+
