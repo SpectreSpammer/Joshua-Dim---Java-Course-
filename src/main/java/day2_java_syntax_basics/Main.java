2b5ae0d0807efe29807b089ea3e9615b8847fa62
SpectreSpammer <rnavarra.business@gmail.com>
2024-11-16 01:27:39 +0800

    Another commit message

--- src/main/java/day2_java_syntax_basics/Main.java
+++ src/main/java/day2_java_syntax_basics/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome Joshua to Java!.");
+        System.out.println("Hi Joshua to Java!.");
         System.out.println("hello world");
 
     }
