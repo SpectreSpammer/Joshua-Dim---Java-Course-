b650aa543a145de5a43447660ebe3d4def0922d5
SpectreSpammer <rnavarra.business@gmail.com>
2024-11-17 00:04:22 +0800

    Nov 17- Day 2 Accomplishments → Day  2: Java Syntax Basics

--- src/main/java/day2_java_syntax_basics/MainJavaBasics.java
+++ src/main/java/day2_java_syntax_basics/MainJavaBasics.java
@@ -1,12 +1,16 @@
 package day2_java_syntax_basics;
 
-public class Main {
+public class MainJavaBasics {
     public static void main(String[] args) {
+
 
         System.out.println("Welcome Joshua to Java!.");
         System.out.println("Hi Joshua to Java!.");
         System.out.println("hello world");
 
+//        Message message = new Message();
+        Message.sayHello();
     }
 }
+
 
---
+++ src/main/java/day2_java_syntax_basics/Message.java
@@ -0,0 +1,9 @@
+package day2_java_syntax_basics;
+
+public class Message {
+
+    public static void sayHello(){
+        System.out.println("Hello Josh!");
+    }
+
+}
